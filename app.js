47ca58a04a19dc5f9a6f8d7d6f8a624aac0b5b56
shamaayil ahmed <shamayilahmad12@gmail.com>
2020-10-06 19:53:47 +0530

    added newrelease and upcoming movies

--- app.js
+++ app.js
@@ -1,36 +1,87 @@
-const APIURL = 'https://api.themoviedb.org/3/discover/movie?api_key=2d21bdf2bd0f6a43f9a0dc995b785d06&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
-const TVAPIURL = 'https://api.themoviedb.org/3/discover/tv?api_key=2d21bdf2bd0f6a43f9a0dc995b785d06&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false';
+const PMURL = 'https://api.themoviedb.org/3/movie/popular?api_key=2d21bdf2bd0f6a43f9a0dc995b785d06&language=en-US&page=1';
+const NMURL='https://api.themoviedb.org/3/movie/now_playing?api_key=2d21bdf2bd0f6a43f9a0dc995b785d06&language=en-US&page=1';
+const UMURL='https://api.themoviedb.org/3/movie/upcoming?api_key=2d21bdf2bd0f6a43f9a0dc995b785d06&language=en-US&page=1';
 
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
 
 
 const toprated=document.getElementById('light-slider');
+const newrelease=document.getElementById('newrelease');
+const upcoming=document.getElementById('upcoming').querySelector('#light-slider');
 
 async function getMovies() {
-  const resp = await fetch(APIURL);
-  const responseJSON = await resp.json();
+  const Presp = await fetch(PMURL);
+  const PresponseJSON = await Presp.json();
 
-  console.log(responseJSON);
+  const Nresp=await fetch(NMURL);
+  const NresponseJSON=await Nresp.json();
 
-  responseJSON.results.forEach(movie => {
+  const Uresp=await fetch(UMURL);
+  const UresponseJSON=await Uresp.json();  
+
+  console.log(PresponseJSON);
+
+  PresponseJSON.results.forEach(movie => {
     const {poster_path,title,vote_average}=movie;
+
     const movieEl = document.createElement('li');
     const movieC = document.createElement('div');
     movieC.classList.add('tmovie');
+
+
     movieC.innerHTML = `
-    <div class="tmovie">
     <img src="${IMGPATH+poster_path}" alt="${title}">
     <div class="tinfo">
       <h5>${title}</h5>
       <span>${vote_average}</span>
     </div>
-    </div>
     `;
     movieEl.appendChild(movieC);
     toprated.appendChild(movieEl);
   });
+// NEW
+  NresponseJSON.results.forEach(movie => {
+    const {poster_path,title,vote_average}=movie;
 
-  return responseJSON;
+    const movieEl = document.createElement('div');
+    
+    movieEl.classList.add('nmovie');
+
+
+    movieEl.innerHTML = `
+    <img src="${IMGPATH+poster_path}" alt="${title}">
+    <div class="ninfo">
+      <h5>${title}</h5>
+      <span>${vote_average}</span>
+    </div>
+    `;
+    newrelease.appendChild(movieEl);
+  });
+
+  // upcoming
+
+  UresponseJSON.results.forEach(movie => {
+    const {poster_path,title,vote_average}=movie;
+
+    const movieEl = document.createElement('li');
+    const movieC = document.createElement('div');
+    movieC.classList.add('umovie');
+
+
+    movieC.innerHTML = `
+    <img src="${IMGPATH+poster_path}" alt="${title}">
+    <div class="uinfo">
+      <h5>${title}</h5>
+      <span>${vote_average}</span>
+    </div>
+    `;
+  
+    movieEl.appendChild(movieC);
+    upcoming.appendChild(movieEl);
+  });
+  return PresponseJSON;
+  return NresponseJSON;
+  return UresponseJSON;
 }
 // async function getTV(){
 //   const resp=await fetch(TVAPIURL);
@@ -61,7 +112,7 @@ $(document).ready(function () {
     speed: 2000,
     pauseOnHover: true,
     pause: 3000,
-    // controls: false
+    controls: false
   });
   slider.play();
 });
